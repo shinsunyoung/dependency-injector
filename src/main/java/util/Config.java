f7f52daaf1ea5ba59cb399a4aa32f24cc8c7daba
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2021-03-16 00:36:43 +0900

    [#3] Config 파일 분리

--- src/main/java/util/Config.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Config {
-
-  public static final String REQUEST_URL = "https://mvnrepository.com/search?q={search}&sort=relevance";
-  public static final String SUB_REQUEST_URL = "https://mvnrepository.com/artifact/{project}/{version}";
-
-}
---
+++ src/main/java/util/UrlConfig.java
@@ -0,0 +1,6 @@
+package util;
+
+public class UrlConfig {
+  public static String DEPENDENCY_REQUEST_URL = "https://mvnrepository.com/search?q=";
+  public static final String SUB_REQUEST_URL = "https://mvnrepository.com/artifact/{project}/{version}";
+}
